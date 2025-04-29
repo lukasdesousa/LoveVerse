72b6f44b0a120801ef0c34a7c60b70b5a6acfd97
= <lukassousa.ls13@gmail.com>
2025-04-28 23:20:57 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,14 @@
 // middleware.ts
 import { NextRequest, NextResponse } from "next/server";
-import { jwtVerify } from "jose";
-import { checkIfEmailIsVerified } from "./hooks/useEmailVerification";
-
-const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET as string);
 
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
+  
   try {
-    const { payload } = await jwtVerify(token, JWT_SECRET);
-    const email = payload.email;
-    // O e-mail do usuário vem do payload do JWT
-    const verification = await checkIfEmailIsVerified(email as string);
-    
-    if(!verification) {
-      return NextResponse.redirect(new URL("/email_verification", req.url));
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", req.url));
     }
-
     return NextResponse.next();
   } catch (error) {
     console.error("Erro no middleware:", error);
@@ -30,5 +17,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/create/:path*"],
+  matcher: ["/create/:path*", "/user/:path*"],
 };
