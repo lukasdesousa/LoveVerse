b6bd966cb39342ce6a41c28990d7fa4079e2ce7d
= <lukassousa.ls13@gmail.com>
2025-05-06 16:03:00 -0300

    Update handle-payment.ts

--- src/app/server/handle-payment.ts
+++ src/app/server/handle-payment.ts
@@ -1,13 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import "server-only";
 
 import { PaymentResponse } from "mercadopago/dist/clients/payment/commonTypes";
 
 export async function handleMercadoPagoPayment(paymentData: PaymentResponse) {
-  const metadata = paymentData.metadata;
+  //const metadata = paymentData.metadata;
   const paymentStatus = paymentData.status;
-  const userEmail = metadata.user_email; // Os metadados do Mercado Pago são convertidos para snake_case
-  const testeId = metadata.teste_id; // Os metadados do Mercado Pago são convertidos para snake_case
+  //const userEmail = metadata.user_email; // Os metadados do Mercado Pago são convertidos para snake_case
+  //const testeId = metadata.teste_id; // Os metadados do Mercado Pago são convertidos para snake_case
 
   if(paymentStatus === 'approved') {
     console.log('O pagamento foi aprovado, redirecionando para a rota...')
