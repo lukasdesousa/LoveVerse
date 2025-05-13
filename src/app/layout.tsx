a52c4e16c2ab23b20271d34aa8d5cf472043550b
= <lukassousa.ls13@gmail.com>
2025-05-13 15:30:28 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,69 @@
 /* eslint-disable react-refresh/only-export-components */
 import { Inter } from "next/font/google";
 import { GlobalStyle } from "@/styles/GlobalStyle";
-import { Metadata } from "next";
+import { Metadata, Viewport } from "next";
 import ReduxProvider from "@/store/reduxProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon.ico',
-    apple: '/apple-touch-icon.png',
+  title: "LoveVerse – Crie mensagens encantadoras e compartilhe amor",
+  description:
+    "Com o LoveVerse, você pode criar e enviar mensagens personalizadas, românticas e criativas para quem ama.",
+  keywords: [
+    "LoveVerse",
+    "mensagens de amor",
+    "cartas românticas",
+    "frases para namorado",
+    "mensagens personalizadas",
+    "romantismo",
+    "conquistar namorada",
+    "site para casais",
+  ],
+  applicationName: "LoveVerse",
+  authors: [{ name: "Lukas De Souza (DEV)" }],
+  alternates: {
+    canonical: "https://www.loveverse.space/",
   },
+  icons: {
+    icon: "/favicon.ico",        // favicon padrão
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  openGraph: {
+    title: "LoveVerse - Envie mensagens de amor inesquecíveis",
+    description:
+      "Descubra a magia de expressar sentimentos com o LoveVerse. Crie mensagens personalizadas e surpreenda quem você ama.",
+    url: "https://www.loveverse.space/",
+    type: "website",
+    images: [
+      {
+        url: "https://res.cloudinary.com/diidbde0o/image/upload/v1747155241/23a5ad20-c297-474b-ae95-ecf42791b1ca_hibbb4.png",
+        width: 1200,
+        height: 630,
+        alt: "LoveVerse – Crie mensagens inesquecíveis",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "LoveVerse - Envie mensagens de amor inesquecíveis",
+    description:
+      "Descubra a magia de expressar sentimentos com o LoveVerse. Crie mensagens personalizadas e surpreenda quem você ama.",
+    images: ["https://res.cloudinary.com/diidbde0o/image/upload/v1747155241/23a5ad20-c297-474b-ae95-ecf42791b1ca_hibbb4.png"],
+    creator: "",
+  },
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 5,
+  userScalable: true,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
