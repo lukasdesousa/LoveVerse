f15ce9d68057c4a2dbb47417976874bb8760d3fd
= <lukassousa.ls13@gmail.com>
2025-06-06 18:02:34 -0300

    Update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,41 @@
+'use client';
+
 import React from "react";
 import { FooterStyle } from "@/styles/components_styles/footer/styled";
 import { Span } from "@/styles/components_styles/mainStyle/styled";
-
+import loveinsta from "public/img/loveinsta.png";
+import lovetok from "public/img/lovetok.png";
+import Image from "next/image";
 
 const Footer: React.FC = () => {
     return <>
         <FooterStyle>
-                    <section>
-                        <p>Love<Span>Verse</Span> - <span style={{fontWeight: '300'}}>Feito com amor</span></p>
+                    <section className="container">
+                        <section>
+                            <p>Love<Span>Verse</Span> - <span style={{fontWeight: '300'}}>Feito com amor</span></p>
+                        </section>
+                        <section className='social-medias'>
+                            <a target="_blank" href="https://www.instagram.com/loveversespace/">
+                                <Image
+                                    src={loveinsta}
+                                    alt="Logo do LoveVerse instagram"
+                                    width={50}
+                                    height={50}
+                                    placeholder="blur"
+                                    quality={100}
+                                />
+                            </a>
+                            <a target="_blank" href="https://www.tiktok.com/@loveverse.space?is_from_webapp=1&sender_device=pc">
+                                <Image
+                                    src={lovetok}
+                                    alt="Logo do LoveVerse tiktok"
+                                    width={50}
+                                    height={50}
+                                    placeholder="blur"
+                                    quality={100}
+                                />
+                            </a>
+                        </section>
                     </section>
         </FooterStyle>
     </>
--- src/styles/components_styles/footer/styled.ts
+++ src/styles/components_styles/footer/styled.ts
@@ -6,9 +6,27 @@ export const FooterStyle = styled.footer<React.CSSProperties>`
     margin: auto;
     border-top: 1px solid rgba(0, 0, 0, 0.53);
     width: 100%;
+
+    .social-medias {
+        display: flex;
+        gap: 5px;
+        margin: auto;
+        justify-content: center;
+        align-items: center;
+
+        a {
+            cursor:  pointer;
+            -webkit-tap-highlight-color: transparent;
+        }
+    }
+
+    .container {
+        margin: 10px auto;
+    }
+    
     
     p {
-        padding: 25px 0;
+        padding: 5px 0;
         font-weight: 600;
         text-align: center;
         font-size: 1rem;
